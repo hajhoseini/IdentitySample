9214881a3cac8707aa002a7ca52d1b6bdd42a941
a.hoseini <a.hoseini@DPK-15>
2024-09-25 16:49:51 +0330

    Login

--- IdentitySample/Controllers/AccountController.cs
+++ IdentitySample/Controllers/AccountController.cs
@@ -8,10 +8,12 @@ namespace IdentitySample.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public AccountController(UserManager<User> userManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -52,6 +54,50 @@ namespace IdentitySample.Controllers
                 message += item.Description + Environment.NewLine;
             }
             TempData["Message"] = message;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            LoginDTO dto = new LoginDTO
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(dto);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDTO dto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+
+            var user = _userManager.FindByNameAsync(dto.UserName).Result;
+
+            _signInManager.SignOutAsync();
+            var result = _signInManager.PasswordSignInAsync(user, dto.Password, dto.IsPersistent, true).Result;
+
+            if(result.Succeeded)
+            {
+                return Redirect(dto.ReturnUrl);
+            }
+
+            if(result.RequiresTwoFactor)
+            {
+                //go to page for TwoFactor
+            }
+
+            if(result.IsLockedOut)
+            {
+                // go to LockedOut
+            }
+
+            ModelState.AddModelError(string.Empty, "login error");
 
             return View();
         }
---
+++ IdentitySample/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentitySample.Models.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool IsPersistent { get; set; } = false;
+
+        public string ReturnUrl { get; set; }
+    }
+}
