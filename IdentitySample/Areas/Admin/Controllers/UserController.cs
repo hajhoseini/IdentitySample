0e5c8d5f575a1773fc5bd66c81418de861041124
a.hoseini <a.hoseini@DPK-15>
2024-10-01 13:42:05 +0330

    User Management - User List

---
+++ IdentitySample/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using IdentitySample.Areas.Admin.Models.DTOs;
+using IdentitySample.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentitySample.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users
+                            .Select(p => new UserListDTO
+                            {
+                                Id = p.Id,
+                                FirstName = p.FirstName,
+                                LastName = p.LastName,
+                                UserName = p.UserName,
+                                PhoneNumber = p.PhoneNumber,
+                                EmailConfirmed = p.EmailConfirmed,
+                                AccessFailedCount = p.AccessFailedCount
+                            }).ToList();
+            return View(users);
+        }
+    }
+}
