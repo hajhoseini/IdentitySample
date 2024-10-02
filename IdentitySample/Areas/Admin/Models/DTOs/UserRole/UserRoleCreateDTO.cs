343f5e1d0b651e7fbf64514e1fa3e16a7282f6b8
a.hoseini <a.hoseini@DPK-15>
2024-10-02 16:45:10 +0330

    UserRole

---
+++ IdentitySample/Areas/Admin/Models/DTOs/UserRole/UserRoleCreateDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IdentitySample.Areas.Admin.Models.DTOs.UserRole
+{
+    public class UserRoleCreateDTO
+    {
+        public string Id { get; set; }
+
+        public string Role { get; set; }
+
+        public List<SelectListItem> Roles { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
