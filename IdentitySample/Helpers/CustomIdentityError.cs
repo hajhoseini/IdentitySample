8d0e5c6a99a9a701a8735dae0c72e09bb0f6b4c3
a.hoseini <a.hoseini@DPK-15>
2024-11-19 11:47:53 +0330

    Persianization identity errors ...

---
+++ IdentitySample/Helpers/CustomIdentityError.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentitySample.Helpers
+{
+    public class CustomIdentityError : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DefaultError),
+                Description = $"طول پسورد باید حداقل {length} کاراکتر باشد"
+            };
+        }
+    }
+}
